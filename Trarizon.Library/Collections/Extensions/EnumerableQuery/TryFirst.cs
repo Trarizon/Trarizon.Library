3c03cd2a5dafc639ed3c785bc69ab6c4f77e439d
Trarizon <44918595+Trarizon@users.noreply.github.com>
2023-12-24 22:49:05 +0800

    Add `Enumerable.TryFirst`

---
+++ Trarizon.Library/Collections/Extensions/EnumerableQuery/TryFirst.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trarizon.Library.Collections.Extensions;
+partial class EnumerableQuery
+{
+    public static bool TryFirst<T>(this IEnumerable<T> source, out T value, T defaultValue = default!)
+    {
+        if (source is IList<T> list) {
+            if (list.Count > 0) {
+                value = list[0];
+                return true;
+            }
+            else {
+                value = defaultValue;
+                return false;
+            }
+        }
+
+        using var enumerator = source.GetEnumerator();
+
+        if (enumerator.MoveNext()) {
+            value = enumerator.Current;
+            return true;
+        }
+        else {
+            value = defaultValue;
+            return false;
+        }
+    }
+}
