a7dbc35a422574c4d915678bc7f4ce7fac9a5b5a
Trarizon <44918595+Trarizon@users.noreply.github.com>
2024-01-28 15:08:05 +0800

    Extension of `Trarizon.TextCommand` for lib

---
+++ Trarizon.Library.TextCommand/Parsers/OptionalParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Trarizon.Library.Wrappers;
+using Trarizon.TextCommand.Parsers;
+
+namespace Trarizon.Library.TextCommand.Parsers;
+public readonly struct OptionalParser<T, TParser>(TParser parser) : IArgParser<Optional<T>> where TParser : IArgParser<T>
+{
+    public bool TryParse(ReadOnlySpan<char> rawArg, [MaybeNullWhen(false)] out Optional<T> result)
+    {
+        if (parser.TryParse(rawArg, out var res)) 
+            result = res;
+        else
+            result = default;
+
+        return true;
+    }
+}
