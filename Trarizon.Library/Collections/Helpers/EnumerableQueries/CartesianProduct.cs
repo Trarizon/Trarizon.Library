b908b26b673072f1250bf43b5127d6758e9e6452
Trarizon <44918595+Trarizon@users.noreply.github.com>
2024-03-10 01:09:15 +0800

    Add CartesianProduct

--- Trarizon.Library/Collections/Helpers/EnumerableQueries/CartesianProduct.cs
+++ Trarizon.Library/Collections/Helpers/EnumerableQueries/CartesianProduct.cs
@@ -1,13 +1,72 @@
-﻿namespace Trarizon.Library.Collections.Helpers;
+﻿using Trarizon.Library.Collections.Helpers.Utilities.Queriers;
+
+namespace Trarizon.Library.Collections.Helpers;
 partial class EnumerableQuery
 {
-    // TODO: Optimize
     public static IEnumerable<(T, T2)> CartesianProduct<T, T2>(this IEnumerable<T> first, IEnumerable<T2> second)
     {
-        foreach (var item in first) {
-            foreach (var item2 in second) {
-                yield return (item, item2);
+        if (first is IList<T> firstList && second is IList<T2> secondList) {
+            return firstList.CartesianProductList(secondList);
+        }
+
+        if (first.TryGetNonEnumeratedCount(out var count) && count == 0) {
+            return Enumerable.Empty<(T, T2)>();
+        }
+        if (second.TryGetNonEnumeratedCount(out count) && count == 0) {
+            return Enumerable.Empty<(T, T2)>();
+        }
+        return new CartesianProductQuerier<T, T2>(first, second);
+    }
+
+    private sealed class CartesianProductQuerier<T, T2>(IEnumerable<T> source, IEnumerable<T2> sub) : SimpleEnumerationQuerier<T, (T, T2)>(source)
+    {
+        private readonly IEnumerable<T2> _subCollection = sub;
+        private IEnumerator<T2>? _subEnumerator;
+
+        public override bool MoveNext()
+        {
+            const int IterateMain = MinPreservedState - 1;
+            const int IterateSub = IterateMain - 1;
+            const int End = IterateSub - 1;
+
+            switch (_state) {
+                case -1:
+                    _enumerator = _source.GetEnumerator();
+                    goto case IterateMain;
+                case IterateMain: // Not state
+                    if (_enumerator!.MoveNext()) {
+                        _current.Item1 = _enumerator.Current;
+                        _subEnumerator = _subCollection.GetEnumerator();
+                        _state = IterateSub;
+                        goto case IterateSub;
+                    }
+                    else {
+                        _state = End;
+                        return false;
+                    }
+                case IterateSub:
+                    if (_subEnumerator!.MoveNext()) {
+                        _current.Item2 = _subEnumerator.Current;
+                        return true;
+                    }
+                    else {
+                        _subEnumerator.Dispose();
+                        // IterateMain state wont return without change _state
+                        // So here is unnecessary to reassign _state
+                        goto case IterateMain;
+                    }
+                case End:
+                default:
+                    return false;
             }
+        }
+
+        protected override EnumerationQuerier<(T, T2)> Clone() => new CartesianProductQuerier<T, T2>(_source, _subCollection);
+
+        protected override void DisposeInternal()
+        {
+            base.DisposeInternal();
+            _subEnumerator?.Dispose();
         }
     }
 }
---
+++ Trarizon.Library/Collections/Helpers/ListQueries/CartesianProduct.cs
@@ -0,0 +1,39 @@
+﻿using Trarizon.Library.Collections.Helpers.ListQueries.Helpers;
+using Trarizon.Library.Collections.Helpers.Utilities.Queriers;
+
+namespace Trarizon.Library.Collections.Helpers;
+partial class ListQuery
+{
+    public static IList<(T, T2)> CartesianProductList<T, T2>(this IList<T> list, IList<T2> list2)
+    {
+        if (list.Count == 0 || list2.Count == 0)
+            return Array.Empty<(T, T2)>();
+        return new CartesianProductQuerier<ListWrapper<T>, ListWrapper<T2>, T, T2>(list.Wrap(), list2.Wrap());
+    }
+
+    public static IReadOnlyList<(T, T2)> GetCartesianProductROList<T, T2>(this IReadOnlyList<T> list, IReadOnlyList<T2> list2)
+    {
+        if (list.Count == 0 || list2.Count == 0)
+            return Array.Empty<(T, T2)>();
+        return new CartesianProductQuerier<IReadOnlyList<T>, IReadOnlyList<T2>, T, T2>(list, list2);
+    }
+
+
+    private sealed class CartesianProductQuerier<TList, TList2, T, T2>(TList list, TList2 list2)
+        : SimpleListQuerier<TList, T, (T, T2)>(list)
+        where TList : IReadOnlyList<T>
+        where TList2 : IReadOnlyList<T2>
+    {
+        public override (T, T2) this[int index]
+        {
+            get {
+                var (div, rem) = Math.DivRem(index, list2.Count);
+                return (_list[div], list2[rem]);
+            }
+        }
+
+        public override int Count => _list.Count * list2.Count;
+
+        protected override EnumerationQuerier<(T, T2)> Clone() => new CartesianProductQuerier<TList, TList2, T, T2>(_list, list2);
+    }
+}
