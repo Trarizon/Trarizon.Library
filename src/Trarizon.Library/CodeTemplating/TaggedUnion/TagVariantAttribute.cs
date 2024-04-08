19ba7e1a5a8d91cb1917e391e8e44bc31238a5e8
Trarizon <44918595+Trarizon@users.noreply.github.com>
2024-04-08 21:08:23 +0800

    Provide default value for TagVariant params

--- src/Trarizon.Library/CodeTemplating/TaggedUnion/TagVariantAttribute.cs
+++ src/Trarizon.Library/CodeTemplating/TaggedUnion/TagVariantAttribute.cs
@@ -12,23 +12,23 @@ public class TagVariantAttribute(params Type[] types) : Attribute
 public sealed class TagVariantAttribute<T>(string? name = null)
     : TagVariantAttribute([typeof(T)], [name]);
 
-public sealed class TagVariantAttribute<T1, T2>(string? name1, string? name2)
+public sealed class TagVariantAttribute<T1, T2>(string? name1 = null, string? name2 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2)], [name1, name2]);
 
-public sealed class TagVariantAttribute<T1, T2, T3>(string? name1, string? name2, string? name3)
+public sealed class TagVariantAttribute<T1, T2, T3>(string? name1 = null, string? name2 = null, string? name3 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2), typeof(T3)], [name1, name2, name3]);
 
-public sealed class TagVariantAttribute<T1, T2, T3, T4>(string? name1, string? name2, string? name3, string? name4)
+public sealed class TagVariantAttribute<T1, T2, T3, T4>(string? name1 = null, string? name2 = null, string? name3 = null, string? name4 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2), typeof(T3), typeof(T4)], [name1, name2, name3, name4]);
 
-public sealed class TagVariantAttribute<T1, T2, T3, T4, T5>(string? name1, string? name2, string? name3, string? name4, string? name5)
+public sealed class TagVariantAttribute<T1, T2, T3, T4, T5>(string? name1 = null, string? name2 = null, string? name3 = null, string? name4 = null, string? name5 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5)], [name1, name2, name3, name4, name5]);
 
-public sealed class TagVariantAttribute<T1, T2, T3, T4, T5, T6>(string? name1, string? name2, string? name3, string? name4, string? name5, string? name6)
+public sealed class TagVariantAttribute<T1, T2, T3, T4, T5, T6>(string? name1 = null, string? name2 = null, string? name3 = null, string? name4 = null, string? name5 = null, string? name6 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6)], [name1, name2, name3, name4, name5, name6]);
 
-public sealed class TagVariantAttribute<T1, T2, T3, T4, T5, T6, T7>(string? name1, string? name2, string? name3, string? name4, string? name5, string? name6, string? name7)
+public sealed class TagVariantAttribute<T1, T2, T3, T4, T5, T6, T7>(string? name1 = null, string? name2 = null, string? name3 = null, string? name4 = null, string? name5 = null, string? name6 = null, string? name7 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7)], [name1, name2, name3, name4, name5, name6, name7]);
 
-public sealed class TagVariantAttribute<T1, T2, T3, T4, T5, T6, T7, T8>(string? name1, string? name2, string? name3, string? name4, string? name5, string? name6, string? name7, string? name8)
+public sealed class TagVariantAttribute<T1, T2, T3, T4, T5, T6, T7, T8>(string? name1 = null, string? name2 = null, string? name3 = null, string? name4 = null, string? name5 = null, string? name6 = null, string? name7 = null, string? name8 = null)
     : TagVariantAttribute([typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7), typeof(T8)], [name1, name2, name3, name4, name5, name6, name7, name8]);
