7d798c9b1a98218784021ca5ad68a47aeeb59e73
Trarizon <44918595+Trarizon@users.noreply.github.com>
2024-10-19 21:01:25 +0800

    Remove Old collection impl, most of `AllocOptXXX`s are removed

--- Trarizon.Library.Collections/_Collections/IByKeyEqualityComparer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Trarizon.Library.Collections;
-internal interface IByKeyEqualityComparer<T, TKey>
-{
-    bool Equals(T val, TKey key);
-    int GetHashCode([DisallowNull] TKey key);
-}
--- Trarizon.Library.Collections/_Collections/ThrowConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Trarizon.Library.Collections;
-internal static class ThrowConstants
-{
-    public const string QuerierImmutable = "Querier is immutable.";
-    public const string SequenceContainsNoElement = "Sequence contains no element.";
-    public const string CollectionIsReadOnly = "Collection is readonly";
-}
