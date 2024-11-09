61a9ce071ff688dea001b81877ea0d3053c6bdfd
Trarizon <44918595+Trarizon@users.noreply.github.com>
2024-11-09 15:43:21 +0800

    Add `Ray`, `TraVector` for `Vector2/3`, `Quaternion`

---
+++ src/Trarizon.Library/Numerics/TraGeometry.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace Trarizon.Library.Numerics;
+public static class TraGeometry
+{
+    public static Vector2 ToNormalized(this Vector2 v)
+        => v / v.Length();
+
+    public static Vector3 ToNormalized(this Vector3 v)
+        => v / v.Length();
+
+    public static Quaternion ToNormalized(this Quaternion q)
+    {
+        var len = q.Length();
+        if (len < float.Epsilon)
+            return Quaternion.Identity;
+        else
+            return new(q.X / len, q.Y / len, q.Z / len, q.W / len);
+    }
+
+    public static Vector3 ToEulerAngles(this Quaternion q)
+    {
+        const float RadToAngle = 180 / float.Pi;
+
+        float sinr_cosp = 2f * (q.W * q.X + q.Y * q.Z);
+        float cosr_cosp = 1f - 2f * (q.X * q.X + q.Y * q.Y);
+        float roll = float.Atan2(sinr_cosp, cosr_cosp) * RadToAngle;
+
+        float sinp = 2f * (q.W * q.Y - q.Z * q.X);
+        float pitch = sinp switch
+        {
+            <= -1f => -90f,
+            >= 1f => -90f,
+            _ => float.Asin(sinp) * RadToAngle,
+        };
+
+        float siny_cosy = 2f * (q.W * q.Z + q.X * q.Y);
+        float cosy_cosy = 1f - 2f * (q.Y * q.Y + q.Z * q.Z);
+        float yaw = float.Atan2(siny_cosy, cosy_cosy) * RadToAngle;
+
+        return new(roll, pitch, yaw);
+    }
+}
