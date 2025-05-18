26e130a7fe1615970ba9c72b633a6ee54f388032
Trarizon <44918595+Trarizon@users.noreply.github.com>
2025-05-18 13:54:35 +0800

    Add game dev proj

---
+++ src/game/Trarizon.Library.GameDev/CooldownTimer.cs
@@ -0,0 +1,42 @@
+﻿namespace Trarizon.Library.GameDev;
+public struct CooldownTimer
+{
+    private readonly float _maxTime;
+    private float _t;
+
+    public readonly float MaxTime => _maxTime;
+
+    public readonly float ElapsedTime => _t;
+
+    public CooldownTimer(float maxTime)
+    {
+        _maxTime = maxTime;
+        _t = 0;
+    }
+
+    public void Reset() => _t = 0;
+
+    public bool Elapse(float deltaTime)
+    {
+        _t += deltaTime;
+        if (_t >= _maxTime) {
+            _t -= _maxTime;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static bool Elapse(ref float timer, float maxTime, float deltaTime)
+    {
+        timer += deltaTime;
+        if (timer >= maxTime) {
+            timer -= maxTime;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
