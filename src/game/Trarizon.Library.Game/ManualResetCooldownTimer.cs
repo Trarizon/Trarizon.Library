2df971359e7c5ccd6f331d456639bdbdb9626f82
Trarizon <44918595+Trarizon@users.noreply.github.com>
2025-06-09 23:44:55 +0800

    Optimize api of `ManualResetCooldownTimer`

--- src/game/Trarizon.Library.Game/ManualResetCooldownTimer.cs
+++ src/game/Trarizon.Library.Game/ManualResetCooldownTimer.cs
@@ -18,14 +18,16 @@ public struct ManualResetCooldownTimer
 
     public void Reset() => _t = 0;
 
-    public bool Elapse(float deltaTime) => Elapse(ref _t, _maxTime, deltaTime);
+    public bool Elapse(float deltaTime) => Elapse(ref _t, _maxTime, deltaTime, false);
+
+    public bool ElapseOrCompleted(float deltaTime) => Elapse(ref _t, _maxTime, deltaTime, true);
 
     public bool ElapseAutoReset(float deltaTime) => CooldownTimer.Elapse(ref _t, _maxTime, deltaTime);
 
-    public static bool Elapse(ref float timer, float maxTime, float deltaTime)
+    public static bool Elapse(ref float timer, float maxTime, float deltaTime, bool returnTrueOnAlreadyCompleted = false)
     {
         if (timer >= maxTime)
-            return false;
+            return returnTrueOnAlreadyCompleted;
 
         timer += deltaTime;
         if (timer >= maxTime) {
