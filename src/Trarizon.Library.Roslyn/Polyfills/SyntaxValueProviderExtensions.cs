a04858d976d3753a90d1666d4d98970824caea36
Trarizon <2223998963@qq.com>
2026-08-30 02:42:08 +0800

    (Roslyn) optimzie FAWMN impl

--- src/Trarizon.Library.Roslyn/Polyfills/SyntaxValueProviderExtensions.cs
+++ src/Trarizon.Library.Roslyn/Polyfills/SyntaxValueProviderExtensions.cs
@@ -1,29 +1,38 @@
 ﻿#if !LATEST_ROSLYN
 
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Trarizon.Library.Roslyn;
 
 namespace Microsoft.CodeAnalysis;
 
 public static class SyntaxValueProviderExtensions
 {
-
     public static IncrementalValuesProvider<T> ForAttributeWithMetadataName<T>(this SyntaxValueProvider provider,
         string fullyQualifiedMetadataName,
         Func<SyntaxNode, CancellationToken, bool> predicate,
         Func<GeneratorAttributeSyntaxContext, CancellationToken, T> transform)
     {
-        return provider.CreateSyntaxProvider(predicate, (context, cancellationToken) =>
-        {
-            var symbol = context.SemanticModel.GetDeclaredSymbol(context.Node, cancellationToken);
-            if (symbol == null)
-                return default;
+        return provider.CreateSyntaxProvider(
+            (node, cancellationToken) =>
+            {
+                if (node is not MemberDeclarationSyntax { AttributeLists.Count: > 0 } member)
+                    return false; // No attribute
+                return predicate(member, cancellationToken);
+            },
+            (context, cancellationToken) =>
+            {
+                var symbol = context.SemanticModel.GetDeclaredSymbol(context.Node, cancellationToken);
+                if (symbol == null)
+                    return default;
 
-            var attrs = symbol.GetAttributeDatasByFullyQualifiedMetadataName(fullyQualifiedMetadataName);
-            if (attrs.Length == 0)
-                return default;
+                var attrs = symbol.GetAttributeDatasByFullyQualifiedMetadataName(fullyQualifiedMetadataName);
+                if (attrs.Length == 0)
+                    return default;
 
-            return new Optional<T>(transform(new(context.Node, symbol, context.SemanticModel, attrs), cancellationToken));
-        }).Where(x => x.HasValue).Select((x, c) => x.Value);
+                return new Optional<T>(transform(new(context.Node, symbol, context.SemanticModel, attrs), cancellationToken));
+            })
+            .Where(x => x.HasValue)
+            .Select((x, c) => x.Value);
     }
 
 }
